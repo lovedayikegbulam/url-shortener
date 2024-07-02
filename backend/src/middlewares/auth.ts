56d2795ce0d12f92a9e69a7d0b4f82d48bac6d29
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-07-02 22:48:45 +0100

    Add: auth middleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,18 @@
+// middleware/auth.ts
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+interface AuthRequest extends Request {
+  user?: any;
+}
+
+export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
