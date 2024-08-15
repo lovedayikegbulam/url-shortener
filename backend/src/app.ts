e78f0cbeb94a43c335d82c544122daacfcf3c873
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-08-15 22:10:24 +0100

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import urlRoutes from "./routes/urlRoutes";
-import authRoutes from './routes/authRoutes';
-import redirectRoute from "./routes/redirectRoute"
+import authRoutes from "./routes/authRoutes";
+import redirectRoute from "./routes/redirectRoute";
 import limiter from "./utils/rateLimiter";
 import connectToMongoDb from "./db/connectToMongoDb";
-import cors from 'cors';
+import cors from "cors";
 import helmet from "helmet";
 import corsOptions from "./utils/corsOptions";
 import redis from "./db/connectToRedis";
@@ -12,7 +12,7 @@ import redis from "./db/connectToRedis";
 //initialize express
 const app = express();
 
-// parse body 
+// parse body
 app.use(express.json());
 
 //security middlewares
@@ -21,9 +21,23 @@ app.use(helmet());
 app.use(limiter);
 
 //Routes
-app.use('', redirectRoute);
-app.use('/api/auth', authRoutes);
+app.use("", redirectRoute);
+app.use("/api/auth", authRoutes);
 app.use("/api", urlRoutes);
+
+// Handle the base route
+app.get("", (req, res) => {
+  res.status(200);
+  res.json("Welcome");
+});
+
+// catch all route
+app.all("*", (req, res) => {
+  res.status(404);
+  res.json({
+    message: "Page Not found",
+  });
+});
 
 //Connect to databases
 connectToMongoDb();
